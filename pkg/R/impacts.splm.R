8e0fc3643a2e9e647c65e0320973268b5cac0199
gpiras (Gianfranco Piras) <gpiras@mac.com>
2022-11-22 16:19:10 +0000

    add the Durbin model to the GMM functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/splm@251 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/impacts.splm.R
+++ pkg/R/impacts.splm.R
@@ -1,4 +1,4 @@
-impacts.splm<-function(obj, listw = NULL, time = NULL, ..., tr=NULL, R=200, type="mult", empirical=FALSE, Q=NULL){
+impacts.splm <- function(obj, listw = NULL, time = NULL, ..., tr=NULL, R=200, type="mult", empirical=FALSE, Q=NULL){
 
 if(is.null(listw) && is.null(tr)) stop("either listw or tr should be provided")
 
